558eb859901e8cde3efe0a5863c1c3479e7cc5a0
Blindello <129384662+Blindello@users.noreply.github.com>
2023-05-25 19:05:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float f (float y) {
+    return ((y * y) * cos(y)) + 1;
+}
+
 int main() {
-   return 0;
+    float y;
+    float a, b, x, err;
+    do{
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
+
+}while (f(a)*f(b)>=0);
+    do{
+        x=(a+b)/2;
+        if (f(x)==0)
+            break;
+            if(f(a)*f(x)<0){
+                a=x;
+            }
+            else{
+                b=x;
+            }
+            err= abs ((b-a)/2);
+
+    }while (err>= 1e-6);
+    cout<<x<<endl;
+
+    return 0;
 }
